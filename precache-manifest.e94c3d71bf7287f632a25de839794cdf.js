0588a895d58afc2168f1af0ad57b301353a1de3b
Ozhey119 <ozhey119@gmail.com>
2020-04-10 14:11:52 +0300

    Updates

---
+++ precache-manifest.e94c3d71bf7287f632a25de839794cdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f719a2e9c2d5088f4ea8c6c963547c3",
+    "url": "/Face-Recognition/index.html"
+  },
+  {
+    "revision": "494c5d189e5e2504d9b0",
+    "url": "/Face-Recognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e4fe2ba7e58237ab2a5a",
+    "url": "/Face-Recognition/static/css/main.9ab7d07b.chunk.css"
+  },
+  {
+    "revision": "494c5d189e5e2504d9b0",
+    "url": "/Face-Recognition/static/js/2.40ccf3c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Face-Recognition/static/js/2.40ccf3c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4fe2ba7e58237ab2a5a",
+    "url": "/Face-Recognition/static/js/main.abddce8e.chunk.js"
+  },
+  {
+    "revision": "e481c0a4f7becad12d0d",
+    "url": "/Face-Recognition/static/js/runtime-main.961b3a65.js"
+  }
+]);
