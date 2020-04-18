72e9196f9a5ab01791dda983c1fa329fcdce0879
Ozhey119 <ozhey119@gmail.com>
2020-04-18 19:33:22 +0300

    added alon face

--- src/components/faceRecognition/FaceRecognition.js
+++ src/components/faceRecognition/FaceRecognition.js
@@ -6,11 +6,19 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ box, imageUrl }) => {
     return (
+          
         <div className='center ma'>
             <div className='absolute mt2'>
                 <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto' />
                 <div>
-                {box.map( (face, index) => <div key = {index++} className='bounding-box' style={{ top: face.topRow, right: face.rightCol, bottom: face.bottomRow, left: face.leftCol }}></div>)}
+                    {box.map((face, index) =>
+                        <div
+                            key={index++}
+                            className='bounding-box'
+                            style={{ top: face.topRow, right: face.rightCol, bottom: face.bottomRow, left: face.leftCol }}
+                        >
+                        <img id='face' alt='' src={'https://i.imgur.com/6GZIeKl.png'}/>   
+                        </div>)}
                 </div>
             </div>
         </div>
